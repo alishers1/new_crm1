beac56fae0090ba9344921bbc6b0591768534e22
Alisher Siddiqov <alisher.siddiqov99@gmail.com>
2023-09-09 17:56:49 +0500

    home_work #11

--- src/Crm/Entities/Client.cs
+++ src/Crm/Entities/Client.cs
@@ -4,6 +4,9 @@ public sealed class Client
 {
     private string? _firstname;
     private string? _lastName; 
+    private string? _phone;
+    private string? _email;
+    private string? _password;
     private short _age; 
     private Gender _gender;
  
@@ -17,10 +20,32 @@ public sealed class Client
         get => _lastName ?? string.Empty; 
         init => _lastName = value is {Length: > 0} ? value : throw new ArgumentOutOfRangeException(nameof(value)); 
     }
-    public string? MiddleName { get; set; }
+    public string? MiddleName { get; init; }
     public short Age { 
         get => _age; 
         init => _age = value > 18 ? value : throw new ArgumentException("Age is lower than 18!"); 
+    }
+    public string? Phone { 
+        get => _phone ?? string.Empty; 
+        init
+        {
+            if (!string.IsNullOrEmpty(value) && value.Length <= 9 && value.All(char.IsDigit))
+            {
+                _phone = value;
+            }
+            else 
+            {
+                throw new ArgumentException("Phone number must be a non-empty string of up to 9 digits");
+            }
+        }
+    }
+    public string? Email {
+        get => _email ?? string.Empty;
+        init => _email = value is { Length: > 0} ? value : throw new ArgumentOutOfRangeException(nameof(value));
+    }
+    public string? Password {
+        get => _password ?? string.Empty;
+        init => _password = value is {Length: > 0} ? value : throw new ArgumentOutOfRangeException(nameof(value));
     }
     public string? PassportNumber { get; init; }
     public Gender Gender { 
--- src/Crm/Services/ClientService.cs
+++ src/Crm/Services/ClientService.cs
@@ -1,8 +1,10 @@
+using System.Runtime.CompilerServices;
 using Crm.Entities;
 namespace Crm.Service;
 
 public sealed class ClientService 
 {
+    private List<Client> _clients = new();
     public Client CreateClient(ClientInfo clientInfo)
     {
         Client newClient = new()
@@ -14,6 +16,9 @@ public sealed class ClientService
             PassportNumber = clientInfo.PassportNumber, 
             Gender = clientInfo.Gender
         };
+
+        _clients.Add(newClient);
+
         return newClient;
     }
 }
